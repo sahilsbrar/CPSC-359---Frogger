1fa6ec255e7acb36bc132f0489b7c6979bb5df91
Nyzean <47280870+Nyzean@users.noreply.github.com>
2021-04-18 21:53:50 -0600

    Add files via upload
    Updated drawImage to fit 7x14 movement layout and to be grid-centered

--- Source/drawImage.c
+++ Source/drawImage.c
@@ -10,15 +10,34 @@
 #include "frogBWD.h"
 #include "frogLWD.h"
 #include "frogRWD.h"
+#include <string.h>	//sounds
 
 
 /* Definitions */
 
+//string hop = Path.Combine(Environment.CurrentDirectory, "hop.wav");
+
+
+//int playSound( char *filename ) {
+	//char command[256];
+	//int status;
+	
+	/* create command to execute */
+	//sprintf( command, "aplay -c 1 -q -t wav %s", filename );
+	
+	/* play sound */
+	//status = system( command );
+	
+	//return status;	
+//}
+
+
+
 struct fbs framebufferstruct;
 
 int level = 1;
-int lastPressedX = 1200;
-int lastPressedY = 538;
+int lastPressedX = 1239;	// was 1200 (offset by +39)
+int lastPressedY = 535;		// was 538 (offset by -3)
 
 
 /* main function */
@@ -29,7 +48,7 @@ int drawBack(int buttonPressed){
 	
 	if(lastPressedY <= 154 && buttonPressed == 5){
 		level = level + 1;
-		lastPressedY = 602;
+		lastPressedY = 599;	// was 602
 	}
 
 	/* initialize a pixel */
@@ -109,9 +128,9 @@ int drawFrog(){
 	pixel = malloc(sizeof(Pixel));
 	int i=0;
 	unsigned int quarter,byte,word;
-	for (int y = 538; y < 602; y++)//30 is the image height
+	for (int y = 535; y < 599; y++)//30 is the image height; was 538,602
 	{
-		for (int x = 1200; x < 1264; x++) // 30 is image width
+		for (int x = 1239; x < 1303; x++) // 30 is image width; was 1200,1264
 		{	
 				pixel->color = frogPtr[i]; 
 				pixel->x = x;
@@ -141,6 +160,7 @@ int moveFrog(int buttonPressed){
 	unsigned int quarter,byte,word;
 
 	if(buttonPressed == 5){ // pressed UP
+		
 		short int *frogPtr=(short int *) frogFWD.pixel_data;
 		lastPressedY = lastPressedY - 64;
 		for (int y = lastPressedY; y < lastPressedY+64; y++)//30 is the image height
@@ -156,9 +176,18 @@ int moveFrog(int buttonPressed){
 					
 			}
 		}
+		
+		//playSound(hop); // plays hop sound; FIX: currently pauses inputs to process!
+		
 	} else if(buttonPressed == 6){ // pressed DOWN
 		short int *frogPtr=(short int *) frogBWD.pixel_data;
-		lastPressedY = lastPressedY + 64;
+		
+		if(lastPressedY != 535){	// was 538
+			lastPressedY = lastPressedY + 64;
+		}
+		
+		//lastPressedY = lastPressedY + 64;
+		
 		for (int y = lastPressedY; y < lastPressedY+64; y++)//30 is the image height
 		{
 			for (int x = lastPressedX; x < lastPressedX+64; x++) // 30 is image width
@@ -176,7 +205,7 @@ int moveFrog(int buttonPressed){
 
 		short int *frogPtr=(short int *) frogLWD.pixel_data;
 
-		if(lastPressedX != 816){
+		if(lastPressedX != 791){	// was 816, then 855 (hIndex was 13, now 14)
 			lastPressedX = lastPressedX - 64;
 		}
 
@@ -198,7 +227,7 @@ int moveFrog(int buttonPressed){
 
 		short int *frogPtr=(short int *) frogRWD.pixel_data;
 
-		if(lastPressedX != 1584){
+		if(lastPressedX != 1623){	// was 1584 (hIndex was 13, now 14)
 			lastPressedX = lastPressedX + 64;
 		}
 
