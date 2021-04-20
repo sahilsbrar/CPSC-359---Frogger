19307ff3d9ac02523b7b7b8d1353ede1e67c6a60
Sahil Brar <47370267+sahilsbrar@users.noreply.github.com>
2021-04-19 20:34:01 -0600

    Add files via upload

---
+++ source/initGPIO.h
@@ -0,0 +1,25 @@
+/*
+CPSC 359 Project (Part 1)
+Winter 2021
+
+Lecturer: Dr. Jalal Kawash
+
+Authors: Paul Serafini (ID# 30077288), Sahil Brar (ID# 30021440)
+
+Assignment Objective(s):
+Create a device driver for the SNES controller.
+
+
+Authors' Note: We were permitted use of this file by a TA, being
+taken from the GPIO-V1.1 folder uploaded under "Project" on d2l.
+
+*/
+
+
+#ifndef initGPIO
+#define initGPIO
+
+unsigned int *getGPIOPtr(void);
+
+
+#endif
