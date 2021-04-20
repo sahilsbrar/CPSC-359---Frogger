1f33c29d17fabfcfd34023ac347dcb8d81854db4
Nyzean <47280870+Nyzean@users.noreply.github.com>
2021-04-19 19:52:36 -0600

    Add files via upload
    Fixed transparency and garbage pixels

---
+++ drawImage.h
@@ -0,0 +1,16 @@
+#ifndef drawImage
+#define drawImage
+
+/* Definitions */
+typedef struct {
+	short int color;
+	int x, y;
+} Pixel;
+
+void drawPixel(Pixel *pixel);
+int drawGameScreen();
+int drawFrames();
+int drawFrog();
+int moveFrog();
+int drawBaddie();
+#endif
